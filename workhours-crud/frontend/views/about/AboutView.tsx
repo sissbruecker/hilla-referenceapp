ad46f03ea8b1bdd6675243d293a70bb8613c4822
Petter Holmström <petter@vaadin.com>
2023-11-09 15:33:48 +0200

    Example case for debugging combo box behavior.

--- workhours-crud/frontend/views/about/AboutView.tsx
+++ workhours-crud/frontend/views/about/AboutView.tsx
@@ -1,9 +1,46 @@
+import {useState} from "react";
+import {ComboBox} from "@hilla/react-components/ComboBox";
+import {TextField} from "@hilla/react-components/TextField";
+import {Button} from "@hilla/react-components/Button.js";
+
+interface Project {
+    id: string;
+    name: string;
+}
+
 export default function AboutView() {
-  return (
-    <div className="flex flex-col h-full items-center justify-center p-l text-center box-border">
-      <img style={{ width: '200px' }} src="images/empty-plant.png" />
-      <h2>This place intentionally left empty</h2>
-      <p>It’s a place where you can grow your own UI 🤗</p>
-    </div>
-  );
+    const [projects, setProjects] = useState<Project[]>([]);
+    const [projectId, setProjectId] = useState<string>();
+    const [selectedProject, setSelectedProject] = useState<Project>();
+
+    function populateProjects() {
+        setProjects([{id: "1", name: "Project 1"}, {id: "2", name: "Project 2"}, {id: "3", name: "Project 3"}]);
+    }
+
+    function clearProjects() {
+        setProjects([]);
+    }
+
+    return (
+        <div className="flex flex-col h-full items-center justify-center p-l text-center box-border">
+            <ComboBox items={projects}
+                      value={projectId}
+                      label="Project"
+                      itemLabelPath="name"
+                      itemValuePath="id"
+                      onValueChanged={(e) => {
+                          console.log(`combo box value changed to ${e.detail.value}`);
+                          setProjectId(e.detail.value);
+                      }}
+                      onSelectedItemChanged={(e) => {
+                          console.log(`combo box selected item changed to ${e.detail.value}`);
+                          setSelectedProject(e.detail.value as Project);
+                      }}/>
+            <TextField label="Project ID" value={projectId} readonly/>
+            <TextField label="Selected Project" value={selectedProject?.name} readonly/>
+            <Button onClick={populateProjects}>Populate Projects</Button>
+            <Button onClick={clearProjects}>Clear Projects</Button>
+            <TextField label="Set Project ID" onValueChanged={(e) => setProjectId(e.detail.value)}/>
+        </div>
+    );
 }
